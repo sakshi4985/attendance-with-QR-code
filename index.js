c1a7ecdda9e53ce33884b96f6088303bb94981bc
Sakshi Gupta <akansha4941@gmail.com>
2025-09-06 21:23:38 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
-// This is free and unencumbered software released into the public domain.
-// See LICENSE.md for more information.
+module.exports = function (xs, fn) {
+    var res = [];
+    for (var i = 0; i < xs.length; i++) {
+        var x = fn(xs[i], i);
+        if (isArray(x)) res.push.apply(res, x);
+        else res.push(x);
+    }
+    return res;
+};
 
-var encoding = require("./lib/encoding.js");
-
-module.exports = {
-  TextEncoder: encoding.TextEncoder,
-  TextDecoder: encoding.TextDecoder,
+var isArray = Array.isArray || function (xs) {
+    return Object.prototype.toString.call(xs) === '[object Array]';
 };
